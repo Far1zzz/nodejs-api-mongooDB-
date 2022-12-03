e46de4e0d47cf0e46b7eb7b76a93c99eeffa8e60
farizz <fairizwalker@gmail.com>
2022-12-03 20:57:35 +0700

    updateVercel

--- routes/mahasiswa.js
+++ routes/mahasiswa.js
@@ -23,4 +23,50 @@ router.post("/", async (req, res) => {
   }
 });
 
+// Read Data
+router.get("/", async (req, res) => {
+  try {
+    const mahasiswa = await Mahasiswa.find();
+    res.json(mahasiswa);
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
+
+// Update Data
+router.put("/:mahasiswaId", async (req, res) => {
+  // tampung input mahasiswa
+  const data = {
+    nama: req.body.nama,
+    alamat: req.body.alamat,
+    tanggalLahir: req.body.tanggalLahir,
+    tempatLahir: req.body.tempatLahir,
+  };
+
+  try {
+    //update data
+    const mahasiswa = await Mahasiswa.updateOne(
+      { _id: req.params.mahasiswaId },
+      data
+    );
+    // response
+    res.json(mahasiswa);
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
+
+// delete data
+router.delete("/:mahasiswaId", async (req, res) => {
+  try {
+    // delete Data
+    const mahasiswa = await Mahasiswa.deleteOne({
+      _id: req.params.mahasiswaId,
+    });
+    // response
+    res.json(mahasiswa);
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
 module.exports = router;
